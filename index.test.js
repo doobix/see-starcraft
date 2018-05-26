8700bdd94c6f3328cc9beefe7a517b92a82f8b18
doobix <doobix@gmail.com>
2018-05-26 12:20:25 -0700

    add simple test

---
+++ index.test.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const SeeStarCraft = require('./index');
+const ssc = new SeeStarCraft();
+
+const randomRace = ssc.getRandomRace();
+console.log(randomRace);
+assert(['Protoss', 'Terran', 'Zerg'].includes(randomRace));
+
+const randomUnit = ssc.getRandomUnit();
+console.log(randomUnit);
+assert(Object.keys(randomUnit).includes('name'));
+assert(Object.keys(randomUnit).includes('cost'));
+assert(Object.keys(randomUnit.cost).includes('minerals'));
+assert(Object.keys(randomUnit.cost).includes('gas'));
+assert(Object.keys(randomUnit.cost).includes('supply'));
+assert(Object.keys(randomUnit.cost).includes('time'));
+
+const randomBuilding = ssc.getRandomBuilding();
+console.log(randomBuilding);
+assert(Object.keys(randomBuilding).includes('name'));
+
