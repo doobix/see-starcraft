7adc9f35fa389db773f286ae129bd166db6dee38
doobix <doobix@gmail.com>
2018-05-25 20:50:26 -0700

    add getRandomBuilding()

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const getRandomInt = require('./random');
+const starCraftBuildings = require('./starcraft-buildings.json');
 const starCraftUnits = require('./starcraft-units.json');
 
 class SeeStarCraft {
+  getRandomBuilding() {
+    const race = this.getRandomRace();
+    const buildingLength = starCraftBuildings[race].length;
+    const randomBuilding = getRandomInt(buildingLength)
+    return starCraftBuildings[race][randomBuilding];
+  }
+
   getRandomRace() {
     const randomRace = getRandomInt(3);
     return Object.keys(starCraftUnits)[randomRace];
